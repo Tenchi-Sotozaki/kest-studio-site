e8722022a001d6ca18189ad5c6b974e73fa1a2ce
SOTOZAKI_TENCHI\tench_ohdi1j0 <tenchi_sotozaki@everyit.co.jp>
2025-12-17 10:08:42 +0900

    ローディングをイベント依存ゼロに変更。

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import App from './App.vue'
 import './style.css'
 
 createApp(App).mount('#app')
+
+const loader = document.getElementById('app-loading')
+if (loader) {
+  loader.classList.add('is-hidden')
+  setTimeout(() => loader.remove(), 800)
+}
